bfda7c20d91912d5f4c130edd1da2953d0117595
Shahd-Rafik <shahdrafik123123@gmail.com>
2023-03-21 08:15:06 -0700

    put

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+putchar('\n');
+}
+}
