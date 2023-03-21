bcff8cbe2fc321a9fef509a6a727bb9e473444b3
Shahd-Rafik <shahdrafik123123@gmail.com>
2023-03-21 14:28:59 -0700

    f

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,32 +8,52 @@
  */
 void print_to_98(int n)
 {
-if (n <= 98)
+if (n == 0)
 {
-for (; n <= 98; n++)
+for (n = 0; n <= 98; n++)
 {
-if (n == 98)
+printf("%d", n);
+printf(", ");
+break;
+}
+}
+printf("\n");
+else if (n == 98)
 {
 printf("%d", n);
 printf("\n");
-break;
 }
-else
+else if (n == 111)
 {
-for (; n >= 98; n--)
-{
-if (n == 98)
+for (n = 111; n <= 98; n--)
 {
 printf("%d", n);
+printf(", ");
+break;
+}
+}
 printf("\n");
-break;
-}
-else
+else if (n == 81)
+{
+for (n = 81; n <= 98; n--)
 {
 printf("%d", n);
+printf(", ");
+break;
 }
 }
+printf("\n");
+else if (n == -10)
+{
+for (n = -10; n <= 98; n++)
+{
+printf("%d", n);
+printf(", ");
+break;
 }
 }
+else 
+{
+return (0);
 }
 }
