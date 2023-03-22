98d2d49723b9e5577c5a32cf7770adb7b58f447f
Shahd-Rafik <shahdrafik123123@gmail.com>
2023-03-21 17:28:21 -0700

    dd

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,59 +1,45 @@
 #include "main.h"
+
 #include <stdio.h>
 
 /**
- * print_to_98 - all natural nums from n to 98
- * @n: the num to start printing from
- * Return: always 0
- */
+* print_to_98 - all natural numbers from n to 98
+* starting from 0
+* @n:the value of the time table
+*/
+
 void print_to_98(int n)
 {
-if (n == 0)
+if (n <= 98)
 {
-for (n = 0; n <= 98; n++)
+for (; n <= 98; n++)
 {
-printf("%d", n);
-printf(", ");
-break;
-}
-}
-printf("\n");
-else if (n == 98)
+if (n == 98)
 {
 printf("%d", n);
 printf("\n");
-}
-else if (n == 111)
-{
-for (n = 111; n <= 98; n--)
-{
-printf("%d", n);
-printf(", ");
 break;
 }
+else
+{
+printf("%d, ", n);
 }
+}
+}
+else
+{
+for (; n >= 98; n--)
+{
+if (n == 98)
+{
+printf("%d", n);
 printf("\n");
-else if (n == 81)
-{
-for (n = 81; n <= 98; n--)
-{
-printf("%d", n);
-printf(", ");
 break;
 }
-}
-printf("\n");
-else if (n == -10)
+else
 {
-for (n = -10; n <= 98; n++)
-{
-printf("%d", n);
-printf(", ");
-break;
+printf("%d, ", n);
 }
 }
-else 
-{
-return (0);
 }
 }
